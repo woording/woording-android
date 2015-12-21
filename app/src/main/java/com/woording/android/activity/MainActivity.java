4fcbf39838ba1a8253d9ab57e773c46592581b24
PhiliPdB <phlpdbrn@gmail.com>
2015-12-21 19:39:21 +0100

    Changed API location to non local and optimize imports

--- app/src/main/java/com/woording/android/activity/MainActivity.java
+++ app/src/main/java/com/woording/android/activity/MainActivity.java
@@ -20,10 +20,7 @@ import android.os.Handler;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -38,7 +35,6 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.woording.android.App;
 import com.woording.android.List;
 import com.woording.android.ListsListFragment;
-import com.woording.android.ListsViewAdapter;
 import com.woording.android.R;
 import com.woording.android.VolleySingleton;
 import com.woording.android.account.AccountUtils;
@@ -47,9 +43,6 @@ import com.woording.android.account.AuthPreferences;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements ListsListFragment.OnFragmentInteractionListener {
 
