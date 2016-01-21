0884ba1bac32e483a5b8b659dd52e7bd8c6f8dd9
PhiliPdB <phlpdbrn@gmail.com>
2016-01-21 17:34:44 +0100

    Add component ClearableEditText to add clear text button to EditText which closes #24

---
+++ app/src/main/java/com/woording/android/components/ClearableEditText.java
@@ -0,0 +1,119 @@
+package com.woording.android.components;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+
+import com.woording.android.R;
+import com.woording.android.adapter.TextWatcherAdapter;
+
+/**
+ * To change clear icon, set
+ *
+ * <pre>
+ * android:drawableRight="@drawable/custom_icon"
+ * </pre>
+ */
+public class ClearableEditText extends EditText
+        implements View.OnTouchListener, View.OnFocusChangeListener, TextWatcherAdapter.TextWatcherListener {
+
+    public interface Listener {
+        void didClearText();
+    }
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+
+    private Drawable xD;
+    private Listener listener;
+
+    public ClearableEditText(Context context) {
+        super(context);
+        init();
+    }
+
+    public ClearableEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ClearableEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    @Override
+    public void setOnTouchListener(OnTouchListener l) {
+        this.l = l;
+    }
+
+    @Override
+    public void setOnFocusChangeListener(OnFocusChangeListener f) {
+        this.f = f;
+    }
+
+    private OnTouchListener l;
+    private OnFocusChangeListener f;
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (getCompoundDrawables()[2] != null) {
+            boolean tappedX = event.getX() > (getWidth() - getPaddingRight() - xD.getIntrinsicWidth());
+            if (tappedX) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    setText("");
+                    if (listener != null) {
+                        listener.didClearText();
+                    }
+                }
+                return true;
+            }
+        }
+
+        return l != null && l.onTouch(v, event);
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (hasFocus) {
+            setClearIconVisible(!TextUtils.isEmpty(getText()));
+        } else {
+            setClearIconVisible(false);
+        }
+        if (f != null) {
+            f.onFocusChange(v, hasFocus);
+        }
+    }
+
+    @Override
+    public void onTextChanged(EditText view, String text) {
+        if (isFocused()) {
+            setClearIconVisible(!TextUtils.isEmpty(text));
+        }
+    }
+
+    private void init() {
+        xD = getCompoundDrawables()[2];
+        if (xD == null) {
+            xD = getResources().getDrawable(R.drawable.ic_clear_grey_24dp);
+        }
+        xD.setBounds(0, 0, xD.getIntrinsicWidth(), xD.getIntrinsicHeight());
+        setClearIconVisible(false);
+        super.setOnTouchListener(this);
+        super.setOnFocusChangeListener(this);
+        addTextChangedListener(new TextWatcherAdapter(this, this));
+    }
+
+    protected void setClearIconVisible(boolean visible) {
+        boolean wasVisible = (getCompoundDrawables()[2] != null);
+        if (visible != wasVisible) {
+            Drawable x = visible ? xD : null;
+            setCompoundDrawables(getCompoundDrawables()[0], getCompoundDrawables()[1], x, getCompoundDrawables()[3]);
+        }
+    }
+}
