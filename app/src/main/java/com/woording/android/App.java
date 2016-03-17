2ac0952c944142a50f8abcbda0d84d6290d3fce9
PhiliPdB <phlpdbrn@gmail.com>
2016-03-17 18:50:24 +0100

    Setup StrictMode to help with debugging

--- app/src/main/java/com/woording/android/App.java
+++ app/src/main/java/com/woording/android/App.java
@@ -8,6 +8,7 @@ package com.woording.android;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.StrictMode;
 
 public class App extends Application {
 
@@ -23,6 +24,23 @@ public class App extends Application {
         App.context = getApplicationContext();
 
         mDualPane = getResources().getBoolean(R.bool.is_dual_pane);
+
+        if (BuildConfig.DEBUG) {
+            // Enable StrictMode
+            StrictMode.setVmPolicy(
+                    new StrictMode.VmPolicy.Builder()
+                        .detectAll()
+                        .penaltyLog()
+                        .penaltyDeath()
+                        .build()
+            );
+            StrictMode.setThreadPolicy(
+                    new StrictMode.ThreadPolicy.Builder()
+                        .detectAll()
+                        .penaltyLog()
+                        .build()
+            );
+        }
     }
 
     public static Context getAppContext() {
