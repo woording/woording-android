de66259f727f810644c0780be83333526a132363
PhiliPdB <phlpdbrn@gmail.com>
2016-04-24 12:14:13 +0200

    Disable select, copy and paste on EditText in practice

--- app/src/main/java/com/woording/android/components/ClearableEditText.java
+++ app/src/main/java/com/woording/android/components/ClearableEditText.java
@@ -2,8 +2,12 @@ package com.woording.android.components;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
@@ -25,6 +29,7 @@ public class ClearableEditText extends EditText
         void didClearText();
     }
 
+    @SuppressWarnings("unused")
     public void setListener(Listener listener) {
         this.listener = listener;
     }
@@ -32,18 +37,23 @@ public class ClearableEditText extends EditText
     private Drawable xD;
     private Listener listener;
 
+    private final Context context;
+
     public ClearableEditText(Context context) {
         super(context);
+        this.context = context;
         init();
     }
 
     public ClearableEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.context = context;
         init();
     }
 
     public ClearableEditText(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        this.context = context;
         init();
     }
 
@@ -97,16 +107,43 @@ public class ClearableEditText extends EditText
         }
     }
 
+    /**
+     * This is a replacement method for the base TextView class' method of the same name. This
+     * method is used in hidden class android.widget.Editor to determine whether the PASTE/REPLACE popup
+     * appears when triggered from the text insertion handle. Returning false forces this window
+     * to never appear.
+     * @return false
+     */
+    @SuppressWarnings("unused")
+    boolean canPaste() {
+        return false;
+    }
+
+    /**
+     * This is a replacement method for the base TextView class' method of the same name. This method
+     * is used in hidden class android.widget.Editor to determine whether the PASTE/REPLACE popup
+     * appears when triggered from the text insertion handle. Returning false forces this window
+     * to never appear.
+     * @return false
+     */
+    @Override
+    public boolean isSuggestionsEnabled() {
+        return false;
+    }
+
     private void init() {
         xD = getCompoundDrawables()[2];
         if (xD == null) {
-            xD = getResources().getDrawable(R.drawable.ic_clear_grey_24dp);
+            xD = ContextCompat.getDrawable(context, R.drawable.ic_clear_grey_24dp);
         }
         xD.setBounds(0, 0, xD.getIntrinsicWidth(), xD.getIntrinsicHeight());
         setClearIconVisible(false);
         super.setOnTouchListener(this);
         super.setOnFocusChangeListener(this);
         addTextChangedListener(new TextWatcherAdapter(this, this));
+
+        this.setLongClickable(false);
+        this.setCustomSelectionActionModeCallback(new ActionModeCallbackInterceptor());
     }
 
     protected void setClearIconVisible(boolean visible) {
@@ -114,6 +151,28 @@ public class ClearableEditText extends EditText
         if (visible != wasVisible) {
             Drawable x = visible ? xD : null;
             setCompoundDrawables(getCompoundDrawables()[0], getCompoundDrawables()[1], x, getCompoundDrawables()[3]);
+        }
+    }
+
+    /**
+     * Prevents the action bar (top horizontal bar with cut, copy, paste, etc.) from appearing
+     * by intercepting the callback that would cause it to be created, and returning false.
+     */
+    private class ActionModeCallbackInterceptor implements ActionMode.Callback {
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            return false;
+        }
+
+        public void onDestroyActionMode(ActionMode mode) {
+
         }
     }
 }
