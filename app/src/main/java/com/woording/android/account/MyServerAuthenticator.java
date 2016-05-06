a83b15d9714030beebb7b5f9244f858bdf485373
PhiliPdB <phlpdbrn@gmail.com>
2016-05-06 17:19:48 +0200

    Add networking for in-app registration as requested in #21

--- app/src/main/java/com/woording/android/account/MyServerAuthenticator.java
+++ app/src/main/java/com/woording/android/account/MyServerAuthenticator.java
@@ -24,6 +24,59 @@ public class MyServerAuthenticator implements IServerAuthenticator {
     @Override
     public String signUp(String email, String username, String password) {
         // TODO: register new user on the server and return its auth token
+        try {
+            URL url = new URL(App.API_LOCATION + "/register");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(15000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setDoInput(true);
+            urlConnection.setDoOutput(true);
+            urlConnection.setUseCaches(true);
+            urlConnection.setInstanceFollowRedirects(false);
+            // Set the content-type as json --> Important
+            urlConnection.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+
+            // Now create the JSONObject
+            JSONObject data = new JSONObject();
+            data.put("username", username);
+            data.put("password", password);
+            data.put("email", email);
+            // And send the data
+            OutputStream output = urlConnection.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(output, "UTF-8"));
+            writer.write(data.toString());
+            writer.flush();
+            writer.close();
+            output.close();
+            // And now connect to the server
+            urlConnection.connect();
+
+            // Now check the response code
+            if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                InputStream inputStream = urlConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder json = new StringBuilder();
+                String inputLine = "";
+
+                while ((inputLine = bufferedReader.readLine()) != null) {
+                    json.append(inputLine);
+                }
+
+                JSONObject response = new JSONObject(json.toString());
+                if (response.getString("response").contains("ERROR")) {
+                    return null;
+                } else {
+                    // Successfully registered
+                }
+
+                inputStream.close();
+            }
+            urlConnection .disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
