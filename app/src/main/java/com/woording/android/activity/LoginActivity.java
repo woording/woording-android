31c5221dc7e8bf470f0a3bfefe31e8f86e5fce63
PhiliPdB <phlpdbrn@gmail.com>
2016-05-11 19:30:58 +0200

    Handle input forms for registering in app (#21)

--- app/src/main/java/com/woording/android/activity/LoginActivity.java
+++ app/src/main/java/com/woording/android/activity/LoginActivity.java
@@ -14,15 +14,17 @@ import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.woording.android.R;
@@ -34,6 +36,8 @@ import com.woording.android.components.AccountAuthenticatorAppCompatActivity;
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AccountAuthenticatorAppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
 
     public static final String ARG_ACCOUNT_TYPE = "accountType";
     public static final String ARG_AUTH_TOKEN_TYPE = "authTokenType";
@@ -50,11 +54,14 @@ public class LoginActivity extends AccountAuthenticatorAppCompatActivity {
     private boolean mLoggingIn = true;
 
     // UI references.
-    private EditText mUsernameView;
-    private EditText mPasswordView;
-    private EditText mRepeatPasswordView;
-    private EditText mEmailView;
-    private View mProgressView;
+    private TextInputEditText mUsernameViewLogin;
+    private TextInputEditText mPasswordViewLogin;
+    private TextInputEditText mUsernameViewRegister;
+    private TextInputEditText mPasswordViewRegister;
+    private TextInputEditText mRepeatPasswordView;
+    private TextInputEditText mEmailView;
+    private View mLoginProgressView;
+    private View mRegisterProgressView;
     private View mLoginFormView;
     private View mRegisterFormView;
 
@@ -75,17 +82,37 @@ public class LoginActivity extends AccountAuthenticatorAppCompatActivity {
         // Setup ViewPager
         ViewPagerAdapter adapter = new ViewPagerAdapter();
         ViewPager pager = (ViewPager) findViewById(R.id.view_pager);
-        if (pager != null) pager.setAdapter(adapter);
-        else throw new RuntimeException("pager should not be null");
+        if (pager == null) throw new RuntimeException("pager should not be null");
+        pager.setAdapter(adapter);
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d(TAG, "onPageSelected: Changed page to " + position);
+                mLoggingIn = position == 0;
+                // TODO: 5-11-2016 Change text on ActionBar
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
         // Set up the login form.
-        mUsernameView = (EditText) findViewById(R.id.username);
+        mUsernameViewLogin = (TextInputEditText) findViewById(R.id.username);
+        mUsernameViewRegister = (TextInputEditText) findViewById(R.id.username_register);
 
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        mPasswordViewRegister = (TextInputEditText) findViewById(R.id.password_register);
+        mPasswordViewLogin = (TextInputEditText) findViewById(R.id.password);
+        mPasswordViewLogin.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (mLoggingIn && (id == R.id.login || id == EditorInfo.IME_ACTION_DONE)) {
+                if (id == R.id.login || id == EditorInfo.IME_ACTION_DONE) {
                     attemptLogin();
                     return true;
                 }
@@ -93,13 +120,14 @@ public class LoginActivity extends AccountAuthenticatorAppCompatActivity {
             }
         });
 
-        mRepeatPasswordView = (EditText) findViewById(R.id.password_repeat);
+        mRepeatPasswordView = (TextInputEditText) findViewById(R.id.password_repeat);
 
-        mEmailView = (EditText) findViewById(R.id.email);
+        mEmailView = (TextInputEditText) findViewById(R.id.email);
         mEmailView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (!mLoggingIn && (id == R.id.register || id == EditorInfo.IME_ACTION_DONE)) {
+                if (id == R.id.register || id == EditorInfo.IME_ACTION_DONE) {
+                    Log.d(TAG, "onEditorAction: Attempt register from emailView");
                     attemptRegister();
                     return true;
                 }
@@ -107,18 +135,29 @@ public class LoginActivity extends AccountAuthenticatorAppCompatActivity {
             }
         });
 
-        Button mSignInButton = (Button) findViewById(R.id.sign_in_button);
-        if (mSignInButton == null) throw new RuntimeException("mSignButton should not be null");
-        mSignInButton.setOnClickListener(new OnClickListener() {
+        Button signInButton = (Button) findViewById(R.id.sign_in_button);
+        if (signInButton == null) throw new RuntimeException("signButton should not be null");
+        signInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 attemptLogin();
             }
         });
 
+        Button registerButton = (Button) findViewById(R.id.register_button);
+        if (registerButton == null) throw new RuntimeException("registerButton should not be null");
+        registerButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Attempt register from button");
+                attemptRegister();
+            }
+        });
+
         mLoginFormView = findViewById(R.id.login_form);
         mRegisterFormView = findViewById(R.id.register_form);
-        mProgressView = findViewById(R.id.login_progress);
+        mLoginProgressView = findViewById(R.id.login_progress);
+        mRegisterProgressView = findViewById(R.id.register_progress);
     }
 
     /**
@@ -132,27 +171,27 @@ public class LoginActivity extends AccountAuthenticatorAppCompatActivity {
         }
 
         // Reset errors.
-        mUsernameView.setError(null);
-        mPasswordView.setError(null);
+        mUsernameViewLogin.setError(null);
+        mPasswordViewLogin.setError(null);
 
         // Store values at the time of the login attempt.
-        String username = mUsernameView.getText().toString();
-        String password = mPasswordView.getText().toString();
+        String username = mUsernameViewLogin.getText().toString();
+        String password = mPasswordViewLogin.getText().toString();
 
         boolean cancel = false;
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
         if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
+            mPasswordViewLogin.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordViewLogin;
             cancel = true;
         }
 
-        // Check for a valid email address.
+        // Check for a valid username.
         if (TextUtils.isEmpty(username)) {
-            mUsernameView.setError(getString(R.string.error_field_required));
-            focusView = mUsernameView;
+            mUsernameViewLogin.setError(getString(R.string.error_field_required));
+            focusView = mUsernameViewLogin;
             cancel = true;
         }
 
@@ -171,11 +210,71 @@ public class LoginActivity extends AccountAuthenticatorAppCompatActivity {
 
     private void attemptRegister() {
 
+        // Reset errors
+        mPasswordViewRegister.setError(null);
+        mPasswordViewRegister.setError(null);
+        mRepeatPasswordView.setError(null);
+        mEmailView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String username = mUsernameViewRegister.getText().toString();
+        String password = mPasswordViewRegister.getText().toString();
+        String repeatPassword = mRepeatPasswordView.getText().toString();
+        String email = mEmailView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for valid email
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordViewRegister.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordViewRegister;
+            cancel = true;
+        }
+
+        if (!TextUtils.isEmpty(password) && !TextUtils.isEmpty(repeatPassword) && !password.equals(repeatPassword)) {
+            mRepeatPasswordView.setError(getString(R.string.error_invalid_repeat_password));
+            focusView = mRepeatPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid username.
+        if (TextUtils.isEmpty(username)) {
+            mUsernameViewRegister.setError(getString(R.string.error_field_required));
+            focusView = mUsernameViewRegister;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            // TODO: 5-11-2016 Start handler
+        }
     }
 
     private boolean isPasswordValid(String password) {
         // TODO: Replace this with better logic
         return password.length() > 0;
+    }
+
+    private boolean isEmailValid(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
     /**
@@ -187,6 +286,7 @@ public class LoginActivity extends AccountAuthenticatorAppCompatActivity {
         // the progress spinner.
         final int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
         final View view = mLoggingIn ? mLoginFormView : mRegisterFormView;
+        final View progressView = mLoggingIn ? mLoginProgressView : mRegisterProgressView;
 
         view.setVisibility(show ? View.GONE : View.VISIBLE);
         view.animate().setDuration(shortAnimTime).alpha(show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
@@ -196,11 +296,11 @@ public class LoginActivity extends AccountAuthenticatorAppCompatActivity {
             }
         });
 
-        mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-        mProgressView.animate().setDuration(shortAnimTime).alpha(show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+        progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+        progressView.animate().setDuration(shortAnimTime).alpha(show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
-                mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                progressView.setVisibility(show ? View.VISIBLE : View.GONE);
             }
         });
     }
@@ -238,8 +338,8 @@ public class LoginActivity extends AccountAuthenticatorAppCompatActivity {
             showProgress(false);
 
             if (intent.getStringExtra(AccountManager.KEY_AUTHTOKEN) == null) {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
+                mPasswordViewLogin.setError(getString(R.string.error_incorrect_password));
+                mPasswordViewLogin.requestFocus();
             } else {
                 finishLogin(intent);
             }
